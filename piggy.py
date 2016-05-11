da33f6b94f451acb6e0f15a50bc7ac9fe8f723f8
benomatron <benomatron@gmail.com>
2016-05-10 22:49:23 -0700

    piggy

---
+++ piggy.py
@@ -0,0 +1,3 @@
+# Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end
+# of the word and suffixes an ay, or if a word begins with a vowel you just add way to the end.
+# For example, pig becomes igpay, banana becomes ananabay, and aadvark becomes aadvarkway.
